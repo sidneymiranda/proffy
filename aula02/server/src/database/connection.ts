18fd6e8e67c22596f3dd11e05ef82fac95d47bdc
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-08-04 20:47:58 -0300

    Inclusão do gitignore

---
+++ aula02/server/src/database/connection.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+import path from 'path';
+
+
+const db = Knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+    },
+    useNullAsDefault: true,
+});
+
+export default db;
