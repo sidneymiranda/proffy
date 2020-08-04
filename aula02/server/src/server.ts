a8ddd3d4e2e8e1e3f1f6241b82b51f001e4955a7
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-08-04 20:42:05 -0300

    Criação do database

--- aula02/server/src/server.ts
+++ aula02/server/src/server.ts
@@ -1,16 +1,22 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        {name: 'Sidney', age: 32},
-        {name: 'Lucas', age: 28},
-    ]
+app.use(express.json());
 
-    return response.json(users);
+/*
+GET: Buscar ou listar uma informação
+POST: Criar alguma nova informação
+PUT: Atualizar uma informação existente
+DELETE: Deletar uma informação existente
+
+Corpo (Resquest Body): Dados para criação ou atualização de um registro
+Route Params: Identificar qual recurso quer atualizar ou deletar 
+Query Params: Paginação, filtros, ordenação
+*/
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello world'});
 });
-
-
 
 app.listen(3333);
