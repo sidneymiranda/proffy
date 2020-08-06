b5e8a90f57a72e0fdadb7379347ef7eed3e69729
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-08-06 04:25:58 -0300

    Finalização do frontend com todas as funcionalidades desenvolvidas, utilizando React e já conectado ao banckend com o Node

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,31 +2,57 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+
+export interface Teacher {
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+    };
+
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        return api.post('connections', {
+            user_id: teacher.id,
+
+        })
+    }
+
     return(
         <article className="teacher-item">
         <header>
-        <img src="https://avatars1.githubusercontent.com/u/39252967?s=460&u=d01235702a9f8e264f8da3739c0dbda6acc55217&v=4" alt="Sidney Miranda"/>    
+        <img src={teacher.avatar} alt={teacher.name}/>    
             <div>
-                <strong>Sidney Miranda</strong>
-                <span>Química</span>
+                <strong>{ teacher.name }</strong>
+                <span>{ teacher.subject }</span>
             </div>
         </header>            
            <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas experiências.
+                { teacher.bio }
             </p>
         
         <footer>
-           <p> Preço/hora
-            <strong>R$ 85,00</strong></p>
-            <button>
-                <img src={whatsappIcon} alt="Whatsapp"/>
+           <p> 
+            <strong>R$ { teacher.cost }</strong></p>
+            <a  target="_blanck"
+                onClick={createNewConnection} 
+                href={`https://wa.me/${ teacher.whatsapp }`}
+            >
+                <img src={ whatsappIcon } alt="Whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     );
--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-
 function TeachersList() {
+    const [teachers, setTeachers] = useState([]);
+
+    const [ subject, setSubject] = useState('');
+    const [ week_day, setWeekDay] = useState('');
+    const [ time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+         const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+        setTeachers(response.data);
+    }
+
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis" >               
-                <form action="" id="search-teachers">
+                <form action="" id="search-teachers" onSubmit={searchTeachers}>
                     <Select 
                         name="subject" 
                         label="Disciplina"
                         optionLabel="Selecione" 
+                        value={subject}
+                        onChange={(e) => { setSubject(e.target.value)} }
                         options = {[
                             {value: 'Português', label: 'Português' },
                             {value: 'História', label: 'História' },
@@ -27,10 +50,13 @@ function TeachersList() {
                             {value: 'Física', label: 'Física' },
                         ]}
                     />
+
                     <Select
                         name="week_day"
                         label="Dia da Semana"
                         optionLabel="Selecione"
+                        value={week_day}
+                        onChange={(e) => { setWeekDay(e.target.value)} }
                         options={[
                             {value: '0', label: 'Domingo' },
                             {value: '1', label: 'Segunda' },
@@ -40,18 +66,25 @@ function TeachersList() {
                             {value: '5', label: 'Sexta-feira' },
                             {value: '6', label: 'Sábado' },
                         ]}   
-                    />                           
-                    <Input name="time" type="time" label="Hora"/>
-                
+                    />  
+
+                    <Input 
+                        name="time" 
+                        type="time" 
+                        label="Hora"
+                        value={time}
+                        onChange={(e) => { setTime(e.target.value) }}
+                    />
+
+                    <button type="submit">Buscar</button>                
                 </form>
             </PageHeader>
 
             <main>
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
-               <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={ teacher.id } teacher={teacher} />
+                })}
+
             </main>          
     </div>
     )
