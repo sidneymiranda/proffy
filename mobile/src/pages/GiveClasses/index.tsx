82fbeed8ddeebc385566f45014f054e74e55fff5
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-08-07 00:38:25 -0300

    Conclusão da página GiveClasses com retorno à Landing page via botão

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+
+
+function GiveClasses() {
+    const { goBack } = useNavigation();
+
+    function handleNavigateBack () {
+        goBack();
+    }
+
+
+    return( 
+        <View style={styles.container}>
+            <ImageBackground
+                resizeMode= "contain"
+                source={giveClassesBgImage} 
+                style={styles.content}
+            >
+                <Text style={styles.title}>
+                    Quer ser um Proffy?
+                </Text>
+
+                <Text style={styles.description}>
+                    Para começar, você precisa se cadastrar como professor em nossa plataforma web.
+                </Text>
+            </ImageBackground>
+
+            <RectButton 
+                style={styles.okButton}
+                onPress={handleNavigateBack}
+            >
+                <Text style={styles.okButtonText}>
+                    Tudo bem.
+                </Text>
+            </RectButton>
+        </View>
+    );
+}
+
+export default GiveClasses;
